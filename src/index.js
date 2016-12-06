861d25095bc776ee571b9573c7dcb54681459260
Johanna Tchon <jktchon@gmail.com>
2016-12-06 12:42:17 -0800

    removed routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+// import App2 from './components/App2.jsx';
 import allRecipes from './components/allRecipesData.js'
 
 
-ReactDOM.render(<App recipes={allRecipes}/>, document.getElementById('app'));
+ReactDOM.render(
+  <App />
+  , document.getElementById('app')
+);
