2fcad30c2d356a63d4ebb3375e0cd11798713cc4
Johanna Tchon <jktchon@gmail.com>
2016-12-06 16:46:30 -0800

    added remove button

--- src/components/recipeEntry.jsx
+++ src/components/recipeEntry.jsx
@@ -3,6 +3,38 @@ import React from 'react';
 class RecipeEntry extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: this.props.recipe.name,
+      description: this.props.recipe.description,
+      recipeUrl: this.props.recipe.recipeUrl,
+      photoUrl: this.props.recipe.recipeUrl
+    }
+    this.handleRemoveClick = this.handleRemoveClick.bind(this);
+  }
+
+  handleRemoveClick() {
+    this.props.handleInputChange();
+    // fetch('/remove', {
+    //   method: 'post',
+    //   body: JSON.stringify({name: this.state.name})
+    // }).then(function(data) {
+    //   return data.json();
+    // }).catch(function(err) {
+    //   console.log(err);
+    // })
+    fetch("/remove",
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({name: this.state.name})
+    }).then(function(res){
+      console.log(res);
+    }).catch(function(res){
+      console.log(res);
+    })
   }
 
   render() {
@@ -15,8 +47,12 @@ class RecipeEntry extends React.Component {
           <h4 className="recipe-name">{this.props.recipe.name}</h4>
           <div className="recipe-description"><strong>Description: </strong>{this.props.recipe.description}</div>
           <div className="recipe-recipe">
-            <a href={this.props.recipe.recipeUrl}>Go to Recipe</a>
+            <a href={this.props.recipe.recipeUrl} className="btn btn-info">Go to Recipe</a>
           </div>
+          <div className="recipe-remove">
+            <button className="btn btn-danger pull-right" onClick={this.handleRemoveClick}>remove</button>
+          </div>
+
         </div>
       </div>
     )
