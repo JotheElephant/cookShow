0204d39769870f7c9c4d950abbd4072a6172de78
Johanna Tchon <jktchon@gmail.com>
2016-12-06 16:43:42 -0800

    added remove button request handler

--- server.babel.js
+++ server.babel.js
@@ -1,36 +1,45 @@
-var  express = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
+var db = require('./data/config.js');
+var Recipe = require('./data/recipeModel.js');
 
 
 const app = express();
 
-var data = [{name: 'Paprika Chicken', description: 'While this is a pretty straightforward grilled chicken recipe, the vinaigrette at the end was a last minute touch that worked out nicely. I really like oil and vinegar-based sauces on grilled meats. They work beautifully with the smoky, caramelized exterior.', recipeUrl: 'http://allrecipes.com/recipe/221093/good-frickin-paprika-chicken/print/?recipeType=Recipe&servings=6', photoUrl: 'http://kingofwallpapers.com/chicken/chicken-011.jpg'}];
+//sample data
+// var data = [{name: 'Paprika Chicken', description: 'While this is a pretty straightforward grilled chicken recipe, the vinaigrette at the end was a last minute touch that worked out nicely. I really like oil and vinegar-based sauces on grilled meats. They work beautifully with the smoky, caramelized exterior.', recipeUrl: 'http://allrecipes.com/recipe/221093/good-frickin-paprika-chicken/print/?recipeType=Recipe&servings=6', photoUrl: 'http://kingofwallpapers.com/chicken/chicken-011.jpg'}];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', express.static('public'));
 
+//retrieves all recipes from database
 app.get('/recipes', function(req, res) {
-  //redirected from post to render
-  //console.log(data);
-  res.send(data);
+  Recipe.find({}).exec(function(err, recipes) {
+    res.status(200).send(recipes);
+  })
 });
 
-app.get('/addrecipe', function(req, res) {
-  //add entry to data
-  // console.log(req.body);
-  // data.push(req.body);
-  // Promise.promisify(fs.readFile).then
-  res.redirect('/');
+//recipe is added to the list
+app.post('/addrecipe', function(req, res) {
+  var newRecipe = new Recipe({
+    name: req.body.name,
+    description: req.body.description,
+    recipeUrl: req.body.recipeUrl,
+    photoUrl: req.body.photoUrl
+  });
+  newRecipe.save(function(err, updatedRecipe) {
+    res.redirect('/');
+  });
 })
 
-app.post('/addrecipe', function(req, res) {
-  console.log("post received", req)
-  //add entry to data
-  //console.log(req.body.description);
-  data.push(req.body);
-  res.redirect('/');
-  // res.sendStatus(201);
+//removes recipe from list
+app.post('/remove', function(req, res) {
+  console.log(req.body.name);
+  Recipe.remove({name: req.body.name}, function (err) {
+    console.log('Disappeared!');
+      // if no error, your model is removed
+  });
 })
 
 app.get('*', function (request, response){
